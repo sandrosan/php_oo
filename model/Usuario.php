16e72a8515b9cbc150182826f1c25e818237bbaa
Gustavo <gustavo.softwares@hotmail.com>
2016-06-18 06:54:47 -0300

    scriacao de classe model

---
+++ model/Usuario.php
@@ -0,0 +1,20 @@
+<?php
+
+
+class Usuario {
+   public $email;
+   public $nome;
+   public $nascimento;
+   
+   public function autenticar(){
+       echo 'usuario autenticou no sistema';
+   }
+   
+   public function sair(){
+       echo 'usuario saiu no sistema';
+   }
+
+}
+  
+   
+
